d3bef91975be77e668c4b60def60b817c1d3a604
sjpark <tjdwns9656@naver.com>
2023-02-13 17:57:14 +0900

    Game 구현을 위한 클래스 생성 및 기본 멤버함수 세팅

---
+++ CGameEdu01.cpp
@@ -0,0 +1,26 @@
+#include "CGameEdu01.h"
+
+CGameEdu01::CGameEdu01(void)
+{
+}
+
+CGameEdu01::~CGameEdu01(void)
+{
+}
+
+void CGameEdu01::OnInit()
+{
+}
+
+void CGameEdu01::OnRender()
+{
+}
+
+void CGameEdu01::OnUpdate()
+{
+}
+
+void CGameEdu01::OnRelease()
+{
+}
+
